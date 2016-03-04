ffd7ab0a837370b2d7e2be0f1ea88710f5a086b0
pauldendulk <pauldendulk@gmail.com>
2016-03-04 14:54:29 +0100

    Adding initial code

---
+++ samples/VectorTileSample/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using BruTile;
+using BruTile.Predefined;
+using Mapsui.Layers;
+using VectorTileToBitmapRenderer;
+
+namespace VectorTileSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //MapControl.Map.Layers.Add(new TileLayer(KnownTileSources.Create()));
+            MapControl.Map.Layers.Add(new TileLayer(CreateVectorTileTileSource()));
+        }
+
+        public ITileSource CreateVectorTileTileSource()
+        {
+            return new HttpVectorTileSource(new GlobalSphericalMercator(),
+            //   "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", new[] { "a", "b", "c" }, 
+            //"http://basemapsbeta.arcgis.com/arcgis/rest/services/World_Basemap/VectorTileServer/tile/{z}/{x}/{y}.pbf",
+            "https://vector.mapzen.com/osm/all/{z}/{x}/{y}.mvt?api_key=vector-tiles-LM25tq4",
+            name: "vector tile");
+        }
+    }
+}
---
+++ src/VectorTileToBitmapRenderer/HttpTileSource.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) BruTile developers team. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using BruTile;
+using BruTile.Cache;
+using BruTile.Web;
+
+namespace VectorTileToBitmapRenderer
+{
+    /// <summary>
+    /// This is a custom version of BruTile's HttpTileSource. It has virtual GetTile 
+    /// method. This will be part of the BruTile release after 0.13.0
+    /// </summary>
+    public class HttpTileSourceCustom : ITileSource, IRequest
+    {
+        private readonly WebTileProvider _provider;
+        private readonly ITileSchema _tileSchema;
+
+        public  HttpTileSourceCustom(ITileSchema tileSchema, string urlFormatter, IEnumerable<string> serverNodes = null, 
+            string apiKey = null, string name = null, IPersistentCache<byte[]> persistentCache = null, 
+            Func<Uri, byte[]> tileFetcher = null)
+            : this(tileSchema, new BasicRequest(urlFormatter, serverNodes, apiKey), name, persistentCache, tileFetcher)
+        {
+        }
+
+        public HttpTileSourceCustom(ITileSchema tileSchema, IRequest request, string name = null, 
+            IPersistentCache<byte[]> persistentCache = null, Func<Uri, byte[]> tileFetcher = null)
+        {
+            _provider = new WebTileProvider(request, persistentCache, tileFetcher);
+            _tileSchema = tileSchema;
+            Name = name ?? string.Empty;
+        }
+
+        public IPersistentCache<byte[]> PersistentCache { get { throw new NotImplementedException(); } }
+
+        public Uri GetUri(TileInfo tileInfo)
+        {
+            return _provider.GetUri(tileInfo);
+        }
+
+        public ITileSchema Schema { get { return _tileSchema;  } }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the actual image content of the tile as byte array
+        /// </summary>
+        public virtual byte[] GetTile(TileInfo tileInfo)
+        {
+            return _provider.GetTile(tileInfo);
+        }
+
+
+    }
+}
